55e48fcba7c5908c9dffa8b8805b4d062f977dca
1613066 <zoia.leonardo9@gmail.com>
2018-02-20 15:09:31 +0000

    lab 7 continued

--- LAB_7/form.js
+++ LAB_7/form.js
@@ -1,25 +1,38 @@
 function main () {
 	var a b
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
-	b = + a b;
+
+	b == a + b;
 	c == a * b;
-	
+
 	document.writeln(c);
+}
+
+document.getElementById();
+searchbox = ???;
+
+searchvalue = ???;
+
+if (???) {
+ 	return true;
+}
+ else {
+ 	alert("please type a search query!");
+ 	return false;
 }
 
 // function main () {
 // 	var a, b;
 // 	var c;
-// 	
+//
 // 	a = 10;
 // 	b = 100;
-// 	
+//
 // 	b = a + b;
 // 	c = a * b;
-// 	
+//
 // 	document.writeln(c);
 // }
---
+++ LAB_7/myscript.js
@@ -0,0 +1,9 @@
+
+
+
+function goAgain(time) {
+    if (lastTime != null){
+       onclick
+    }
+}
+requestAnimationFrame(animate);
