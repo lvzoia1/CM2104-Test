6905ec3b862b009668c13b43ac92bdb038d5db0f
1613066 <zoia.leonardo9@gmail.com>
2018-03-05 15:47:35 +0000

    Finished task 3

--- LAB_9/Task_2/mouseevents_jq.js
+++ LAB_9/Task_2/mouseevents_jq.js
@@ -9,19 +9,25 @@ $(function(){
 	// use the html attribute to set the text
 	$("#button1").mouseenter(function(){
 		$("#button1").html("<p>Thank you</p>");
+		$("#button1").css('background-color','lightgreen');
 	});
 
 	$("#button1").mouseleave(function(){
 		$("#button1").html("<p>Mouse Over Me</p>");
+		$("#button1").css('background-color','pink');
+
 	});
 
 	$("#button2").mousedown(function(){
 		$("#button2").html("<p>AAAARGH! pls leave</p>");
+		$("#button2").css('background-color','red');
 	});
 
 	$("#button2").mouseup(function(){
-		$("#button2").html("<p>Phew</p>");
+		$("#button2").html("<p>Phew...</p>");
+		$("#button2").css('background-color','lightblue');
 	});
+
 
 	// now you need to do the same for button1 for mouseleave
 	// and mouseup, mousedown for button2
