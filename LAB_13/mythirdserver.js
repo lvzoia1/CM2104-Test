22ecd3fa25e4a697786fa94189a996b31331aeb6
1613066 <zoia.leonardo9@gmail.com>
2018-03-20 15:51:04 +0000

    lab 13 contd

--- LAB_13/mythirdserver.js
+++ LAB_13/mythirdserver.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var oneLinerJoke = require(‘one-liner-joke’);
+var knockknock = require (‘knock-knock-jokes’);
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("The date and time are currently: " + currentdate.myDateTime());
-   res.end('randomJoke.body');
+   var randomJoke = knockknock();
+   res.end(randomJoke);
 }).listen(8080);
