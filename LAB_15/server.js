95b7d5ee270fa3387a07c85af3a3f4a11a057587
1613066 <zoia.leonardo9@gmail.com>
2018-04-09 16:10:43 +0100

    HA

--- LAB_15/server.js
+++ LAB_15/server.js
@@ -1,10 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}))
 var db;
 
