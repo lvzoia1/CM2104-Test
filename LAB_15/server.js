78d8ff03bcb6db040aa8807fc3919344d341d87b
1613066 <zoia.leonardo9@gmail.com>
2018-04-12 09:51:10 +0100

    maybe I'm a man, maybe I'm a lonely man who's in the middle of somethin', and he doesn't really understand (ta ta ta tata tata tata)

--- LAB_15/server.js
+++ LAB_15/server.js
@@ -18,9 +18,7 @@ MongoClient.connect(url, function(err, database){
 app.get('/all', function(req, res) {
    db.collection('quotes').find().toArray(function(err, result) {
      if (err) throw err;
-
      var output = "<h1>All the quotes</h1>";
-
      for (var i = 0; i < result.length; i++) {
        output += "<div>"
        output += "<h3>" + result[i].name + "</h3>"
@@ -38,3 +36,24 @@ app.post('/quotes', function (req, res) {
      res.redirect('/')
    })
 })
+
+app.post('/search', function(req, res) {
+   db.collection('quotes').find(req.body).toArray(function(err, result) {
+     if (err) throw err;
+     var output = "<h1>All the quotes</h1>";
+     for (var i = 0; i < result.length; i++) {
+       output += "<div>"
+       output += "<h3>" + result[i].name + "</h3>"
+       output += "<p>" + result[i].quote + "</p>"
+       output += "</div>"
+     }
+     res.send(output);
+   });
+});
+
+app.post('/delete', function(req, res) {
+   db.collection('quotes').deleteOne(req.body, function(err, result) {
+     if (err) throw err;
+     res.redirect('/');
+   });
+});
